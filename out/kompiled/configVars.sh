b611fce1edf9f23b475b3a561605356cc02de7e0
scab24 <git.seco@protonmail.com>
2024-01-09 19:21:18 +0100

    h

---
+++ out/kompiled/configVars.sh
@@ -0,0 +1,14 @@
+#!/usr/bin/env bash
+
+declaredConfigVar_PGM='EthereumSimulation'
+declaredConfigVar_USEGAS='Bool'
+declaredConfigVar_CHAINID='Int'
+declaredConfigVar_SCHEDULE='Schedule'
+declaredConfigVar_MODE='Mode'
+declaredConfigVars=(
+    'PGM'
+    'USEGAS'
+    'CHAINID'
+    'SCHEDULE'
+    'MODE'
+)
---
+++ out/kompiled/llvm-library/configVars.sh
@@ -0,0 +1,14 @@
+#!/usr/bin/env bash
+
+declaredConfigVar_SCHEDULE='Schedule'
+declaredConfigVar_PGM='EthereumSimulation'
+declaredConfigVar_MODE='Mode'
+declaredConfigVar_USEGAS='Bool'
+declaredConfigVar_CHAINID='Int'
+declaredConfigVars=(
+    'SCHEDULE'
+    'PGM'
+    'MODE'
+    'USEGAS'
+    'CHAINID'
+)
